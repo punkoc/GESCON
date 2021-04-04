927881b6551b76a00cbc0c4201e24c51f2256459
Carlos Saraiva <carlos@evolucaoinfo.com.br>
2021-04-04 17:14:31 -0300

    Create Swagger

--- gescon-db/index.js
+++ gescon-db/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const http = require('http');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger/swagger_output.json')
 
 const app = express();
 app.use(express.json());
 app.use("/api", routes);
 
-app.listen(8888, (request, response)=> {
-    console.log("Serviço iniciado na porta 8888");
-});
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+http.createServer(app).listen(8888);
+console.log("Listening at:http://localhost:%s/api (HTTP)", 8888);
